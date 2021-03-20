6e91aa4b52db0d72b0fb8bc06c25979d677d5ce0
seajensen <seajensen@gmail.com>
2021-03-20 16:22:40 -0600

    fixed hook issues

--- src/App.js
+++ src/App.js
@@ -15,14 +15,6 @@ function App() {
   const countArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
   const countArray2 = [1, 2, 3, 4, 5, 6, 7]
 
-  const refreshForecast = async () => {
-    let url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + currentLat.toString() + '&lon=' + currentLng.toString() + '&appid=60e806425103fe7664141785c6679a6d&units=' + unit
-    console.log('URL ', url)
-    await fetch(url)
-    .then(res => res.json())
-    .then(data => setForecast(data));
-  }
-
   const hourForecast = () => {
     let myDate = new Date()
     myDate = myDate.getHours()
@@ -60,10 +52,26 @@ function App() {
   }
 
   useEffect(() => {
+    const refreshForecast = async () => {
+      let url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + currentLat.toString() + '&lon=' + currentLng.toString() + '&appid=60e806425103fe7664141785c6679a6d&units=' + unit
+      console.log('URL ', url)
+      await fetch(url)
+      .then(res => res.json())
+      .then(data => setForecast(data));
+    }
+
     refreshForecast()
   }, [currentLat]);
 
   useEffect(() => {
+    const refreshForecast = async () => {
+      let url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + currentLat.toString() + '&lon=' + currentLng.toString() + '&appid=60e806425103fe7664141785c6679a6d&units=' + unit
+      console.log('URL ', url)
+      await fetch(url)
+      .then(res => res.json())
+      .then(data => setForecast(data));
+    }
+
     refreshForecast()
   }, [unit]);
 
@@ -80,33 +88,33 @@ function App() {
         <div 
           className="temp-select"
           onClick={() => {
-            if(unit == 'imperial') {
+            if(unit === 'imperial') {
               setUnit('metric')
             } else {
               setUnit('imperial')
             }
           }}
         >
-          <div style={unit == 'imperial' ? unitStyle : null}><p>F</p></div>
-          <div style={unit == 'metric' ? unitStyle : null}><p>C</p></div>
+          <div style={unit === 'imperial' ? unitStyle : null}><p>F</p></div>
+          <div style={unit === 'metric' ? unitStyle : null}><p>C</p></div>
         </div>
         <h3>Select Location</h3>
         <MapSelection setCurrentLng={setCurrentLng} setCurrentLat={setCurrentLat} />
         <h2>Forecast</h2>
         {forecast ? 
           <div>
-            <p>Current Weather: {forecast.current.temp}&#730;{unit == 'imperial' ? 'F' : 'C'} {forecast.current.weather[0].main}</p>
+            <p>Current Weather: {forecast.current.temp}&#730;{unit === 'imperial' ? 'F' : 'C'} {forecast.current.weather[0].main}</p>
             <h4>Hourly Forecast: </h4>
             <ul style={{listStyle: 'none', paddingLeft: 0}}>
             {countArray.map((value, index) => {
               let hours = hourForecast()
-              return <li key={index} style={{display: 'flex', justifyContent: 'space-between', maxWidth: '40%', margin: 'auto'}}><span>{hours[index]}:</span> {forecast.hourly[index].temp}&#730;{unit == 'imperial' ? 'F' : 'C'} {forecast.hourly[index].weather[0].main}</li>
+              return <li key={index} style={{display: 'flex', justifyContent: 'space-between', maxWidth: '40%', margin: 'auto'}}><span>{hours[index]}:</span> {forecast.hourly[index].temp}&#730;{unit === 'imperial' ? 'F' : 'C'} {forecast.hourly[index].weather[0].main}</li>
             })}
             </ul>
             <h4>Daily Forecast:</h4>
             <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around'}}>{countArray2.map((value, index) => {
               let days = dayForecast()
-              return <div key={index} className="day-card"><p>{days[index]}</p><p>{forecast.daily[index].temp.day}&#730;{unit == 'imperial' ? 'F' : 'C'}</p><p>{forecast.daily[index].weather[0].main}</p></div>
+              return <div key={index} className="day-card"><p>{days[index]}</p><p>{forecast.daily[index].temp.day}&#730;{unit === 'imperial' ? 'F' : 'C'}</p><p>{forecast.daily[index].weather[0].main}</p></div>
             })}</div>
           </div> 
         : null}
